33766dfcee6caa0146643a53c9e89c5f3bdb0e89
Mathias Quintero <me@quintero.io>
2020-12-11 11:05:06 +0100

    Adding session object

---
+++ Sources/Client/Game Logic/Session.swift
@@ -0,0 +1,68 @@
+
+import TokamakDOM
+import CombineShim
+import Foundation
+import Model
+
+class Session: ObservableObject {
+    private let url: URL
+
+    @Published
+    private(set) var game: Game?
+
+    @Published
+    private(set) var error: GameError?
+    private var cancellables: Set<AnyCancellable> = []
+
+    init(url: URL) {
+        self.url = url
+    }
+
+    convenience init(id: GameID?, url: URL) {
+        self.init(url: url)
+        if let id = id {
+            join(id: id)
+        }
+    }
+
+    private func startGame() -> Game {
+        cancellables = []
+        let socket = WebSocket(url: url)
+        socket
+            .onClose()
+            .sink { [unowned self] in
+                self.game = nil
+            }
+            .store(in: &cancellables)
+
+        let game = Game(webSocket: socket)
+        self.game = game
+
+        game
+            .$error
+            .sink { [unowned self] error in self.error = error }
+            .store(in: &cancellables)
+
+        return game
+    }
+
+    func join(id: GameID) {
+        if let game = game {
+            game.join(id: id)
+        } else {
+            startGame().join(id: id)
+        }
+    }
+
+    func configure(rounds: Int) {
+        if let game = game {
+            game.configure(rounds: rounds)
+        } else {
+            startGame().configure(rounds: rounds)
+        }
+    }
+
+    func clearError() {
+        self.error = nil
+    }
+}
