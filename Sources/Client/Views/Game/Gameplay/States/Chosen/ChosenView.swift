e85e94acbc03dbe7077a175472d5fdfcce3070c3
Mathias Quintero <me@quintero.io>
2020-12-16 22:01:39 +0100

    Making chosen view adaptable to size changes

--- Sources/Client/Views/Game/Gameplay/States/Chosen/ChosenView.swift
+++ Sources/Client/Views/Game/Gameplay/States/Chosen/ChosenView.swift
@@ -8,46 +8,48 @@ struct ChosenView: View {
     let meme: Game.ChosenMeme
 
     var body: some View {
-        VStack {
-            Spacer().frame(width: 0, height: 64)
+        GeometryReader { proxy in
+            VStack {
+                Spacer().frame(width: 0, height: 64)
 
-            Text("The judge has spoken!").font(.title).foregroundColor(.primary)
-            if meme.judge.id == game.current?.id {
-                Text("I mean, you have spoken...").font(.callout).foregroundColor(.secondary)
-            } else {
+                Text("The judge has spoken!").font(.title).foregroundColor(.primary)
+                if meme.judge.id == game.current?.id {
+                    Text("I mean, you have spoken...").font(.callout).foregroundColor(.secondary)
+                } else {
+                    HStack {
+                        Text("Judge: ").font(.callout).foregroundColor(.secondary)
+                        Text("\(meme.judge.emoji) \(meme.judge.name)").font(.callout).foregroundColor(.secondary)
+                    }
+                }
+
+                Spacer()
+
                 HStack {
-                    Text("Judge: ").font(.callout).foregroundColor(.secondary)
-                    Text("\(meme.judge.emoji) \(meme.judge.name)").font(.callout).foregroundColor(.secondary)
+                    VStack {
+                        Text("Meme:").font(.title3).foregroundColor(.primary)
+                        Spacer().frame(width: 0, height: 4)
+                        Image(meme.image.absoluteString).frame(height: proxy.size.height / 2.5)
+                    }
+
+                    Spacer().frame(width: 16, height: 0)
+
+                    VStack {
+                        Text("Winner:").font(.title3).foregroundColor(.primary)
+                        Spacer().frame(width: 0, height: 8)
+                        CardContentView(card: .text(meme.winner.text), height: proxy.size.height / 3)
+                        Spacer().frame(width: 0, height: 8)
+                        Text("\(meme.winner.player.emoji) \(meme.winner.player.name)").font(.callout)
+                        Text("\(meme.winner.player.winCount) Points").font(.callout)
+                    }
                 }
+
+                Spacer()
+
+                CustomButton("Next", character: .enter) { game.continue() }
+
+                Spacer().frame(width: 0, height: 64)
             }
-
-            Spacer()
-
-            HStack {
-                VStack {
-                    Text("Meme:").font(.title3).foregroundColor(.primary)
-                    Spacer().frame(width: 0, height: 4)
-                    Image(meme.image.absoluteString).frame(height: 360)
-                }
-
-                Spacer().frame(width: 16, height: 0)
-
-                VStack {
-                    Text("Winner:").font(.title3).foregroundColor(.primary)
-                    Spacer().frame(width: 0, height: 8)
-                    CardContentView(card: .text(meme.winner.text), height: 300)
-                    Spacer().frame(width: 0, height: 8)
-                    Text("\(meme.winner.player.emoji) \(meme.winner.player.name)").font(.callout)
-                    Text("\(meme.winner.player.winCount) Points").font(.callout)
-                }
-            }
-
-            Spacer()
-
-
-            CustomButton("Next", character: .enter) { game.continue() }
-
-            Spacer().frame(width: 0, height: 64)
+            .frame(width: proxy.size.width, height: proxy.size.height)
         }
     }
 }
